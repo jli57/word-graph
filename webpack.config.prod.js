197e0b548d405df11082791d281e4058af8ac0de
Jingna Li <jingna13@gmail.com>
2018-11-04 22:49:21 -0800

    Add additional configurations for gh-pages

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: '/',
+    publicPath: "/word-graph/",
     filename: 'bundle.js'
   },
   resolve: {
